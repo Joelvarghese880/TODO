a61a93234491f1329e21ff6682c2dbdba3a9850c
joel <joelvarghese880@gmail.com>
2025-10-19 13:03:59 +0530

    enable the edit button functional to edit task

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add_task/', views.add_task, name='add_task'),
     path('mark_as_done/<int:pk>/',views.mark_as_done,name="mark_as_done"),
     path('mark_as_undo/<int:pk>/',views.mark_as_undo,name="mark_as_undo"),
+    path('edit_task/<int:pk>/',views.edit_task,name="edit_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from todo.models import Task
 
 # Create your views here.
@@ -19,3 +19,16 @@ def mark_as_undo(request, pk):
     task.is_completed = False
     task.save()
     return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save() 
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task
+        }
+        return render(request,'edit_task.html',context)
